85ff9b1f6686c104265fae2eb066ad3bd7d71e6b
avinassh <avinashsajjan@gmail.com>
2014-01-05 10:20:26 +0530

    added exercise 2

---
+++ Programming-Objective-C/exercise-2/prog1.m
@@ -0,0 +1,8 @@
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char *argv[]) {
+    @autoreleasepool {
+        NSLog (@"\nIn Objective-C, lowercase letters are significant.\nmain is where program execution begins.\nOpen and closed braces enclose program statements in a routine.\nAll program statements must be terminated by a semicolon.");
+    }
+    return 0; 
+}
