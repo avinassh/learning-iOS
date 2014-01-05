9224fd4e377a70cbc3925be22a42cd9267a2fabe
avinassh <avinashsajjan@gmail.com>
2014-01-05 10:52:08 +0530

    exec1

---
+++ Programming-Objective-C/chapter-3/exercise-1/prog1.m
@@ -0,0 +1,16 @@
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[]) {
+    @autoreleasepool { 
+        int Int, playNextSong;
+        int _calloc, Xx, alphaBetaRoutine; 
+        int clearScreen, _1312, z;
+        int ReInitialize, _, A$;
+
+        // A$ = 10;
+        // NSLog (@"%d", A$);
+    }
+    return 0; 
+}
+
+// Illegal var names : 6_05
